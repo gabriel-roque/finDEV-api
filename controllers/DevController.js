559732e3c0aa0cd6629b7d4da05e57122f954c7a
gabriel-roque <gabrielrms.dll@gmail.com>
2020-05-29 17:10:53 -0300

    feat: add expection for browser clients websocket

--- controllers/DevController.js
+++ controllers/DevController.js
@@ -1,32 +1,36 @@
-const axios = require('axios')
-const Dev = require('../models/Dev')
-const parseStringAsArray = require('../utils/parseStringAsArray')
-const { findConnections, sendMessage } = require('../websocket')
+const axios = require('axios');
+const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
+const {
+  findConnections,
+  findConnectionsBrowser,
+  sendMessage,
+} = require('../websocket');
 
 module.exports = {
   async index(request, response) {
-    const devs = await Dev.find()
-    return response.json(devs)
+    const devs = await Dev.find();
+    return response.json(devs);
   },
 
   async store(request, response) {
-    const { github_username, techs, latitude, longitude } = request.body
+    const { github_username, techs, latitude, longitude } = request.body;
 
-    let dev = await Dev.findOne({ github_username })
+    let dev = await Dev.findOne({ github_username });
 
     if (!dev) {
       const apiResponse = await axios.get(
         `https://api.github.com/users/${github_username}`
-      )
+      );
 
-      const { name = login, avatar_url, bio } = apiResponse.data
+      const { name = login, avatar_url, bio } = apiResponse.data;
 
-      const techsArray = parseStringAsArray(techs)
+      const techsArray = parseStringAsArray(techs);
 
       const location = {
         type: 'Point',
         coordinates: [longitude, latitude],
-      }
+      };
 
       dev = await Dev.create({
         name,
@@ -35,40 +39,46 @@ module.exports = {
         avatar_url,
         techs: techsArray,
         location,
-      })
+      });
 
       // Filter new dev for notification socket
-      const sendSocketMessageTo = findConnections(
+      let sendSocketMessageTo = findConnections(
         { latitude, longitude },
         techsArray
-      )
+      );
 
-      sendMessage(sendSocketMessageTo, 'new-dev', dev)
+      let clientsBrowsers = findConnectionsBrowser();
+
+      sendSocketMessageTo = [...sendSocketMessageTo, ...clientsBrowsers];
+
+      // console.log(sendSocketMessageTo);
+
+      sendMessage(sendSocketMessageTo, 'new-dev', dev);
     }
 
-    return response.json(dev)
+    return response.json(dev);
   },
 
   async update(request, response) {
-    let { github_username } = request.params
+    let { github_username } = request.params;
 
-    const filter = { github_username }
-    const update = request.body
+    const filter = { github_username };
+    const update = request.body;
 
-    let dev = await Dev.findOneAndUpdate(filter, update)
+    let dev = await Dev.findOneAndUpdate(filter, update);
 
-    if (dev === null) dev = { status: '404', msg: 'User not found' }
+    if (dev === null) dev = { status: '404', msg: 'User not found' };
 
-    return await response.json(dev)
+    return await response.json(dev);
   },
 
   async destroy(request, response) {
-    let { github_username } = request.params
+    let { github_username } = request.params;
 
-    let dev = await Dev.findOneAndDelete({ github_username })
+    let dev = await Dev.findOneAndDelete({ github_username });
 
-    if (dev === null) dev = { status: '404', msg: 'User not found' }
+    if (dev === null) dev = { status: '404', msg: 'User not found' };
 
-    return await response.json(dev)
+    return await response.json(dev);
   },
-}
+};
--- websocket.js
+++ websocket.js
@@ -1,38 +1,51 @@
-const socketio = require('socket.io')
-const parseStringAsArray = require('./utils/parseStringAsArray')
-const calculateDistance = require('./utils/calculateDistance')
+const socketio = require('socket.io');
+const parseStringAsArray = require('./utils/parseStringAsArray');
+const calculateDistance = require('./utils/calculateDistance');
 
-let io
-const connections = []
+let io;
+const connections = [];
+const connectionsBrowser = [];
 
-exports.setupWebsocket = server => {
-  io = socketio(server)
+exports.setupWebsocket = (server) => {
+  io = socketio(server);
 
-  io.on('connection', socket => {
-    const { latitude, longitude, techs } = socket.handshake.query
+  io.on('connection', (socket) => {
+    const { latitude, longitude, techs } = socket.handshake.query;
 
-    connections.push({
-      id: socket.id,
-      coordinates: {
-        latitude: Number(latitude),
-        longitude: Number(longitude),
-      },
-      techs: parseStringAsArray(techs),
-    })
-  })
-}
+    // console.log(`New socket: ${socket.id}`);
+
+    if (!socket.handshake.query.hasOwnProperty('browser')) {
+      connections.push({
+        id: socket.id,
+        coordinates: {
+          latitude: Number(latitude),
+          longitude: Number(longitude),
+        },
+        techs: parseStringAsArray(techs),
+      });
+    } else {
+      connectionsBrowser.push({
+        id: socket.id,
+        techs: [],
+      });
+    }
+    // console.log(connectionsBrowser);
+  });
+};
 
 exports.findConnections = (coordinates, techs) => {
-  return connections.filter(connection => {
+  return connections.filter((connection) => {
     return (
       calculateDistance(coordinates, connection.coordinates) < 10 &&
-      connection.techs.some(item => techs.includes(item))
-    )
-  })
-}
+      connection.techs.some((item) => techs.includes(item))
+    );
+  });
+};
+
+exports.findConnectionsBrowser = () => connectionsBrowser;
 
 exports.sendMessage = (to, message, data) => {
-  to.forEach(connection => {
-    io.to(connection.id).emit(message, data)
-  })
-}
+  to.forEach((connection) => {
+    io.to(connection.id).emit(message, data);
+  });
+};
